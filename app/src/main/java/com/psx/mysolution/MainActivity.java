af1da209e3cdb6a64e87d736539eafe839c60cbc
psx95 <pranav.ps95@hotmail.com>
2017-05-09 02:59:38 +0530

    Cleaning Code for major changes

--- app/src/main/java/com/psx/mysolution/MainActivity.java
+++ app/src/main/java/com/psx/mysolution/MainActivity.java
@@ -20,6 +20,7 @@ import com.squareup.okhttp.OkHttpClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.Map;
@@ -28,9 +29,11 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.functions.Func2;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -123,7 +126,26 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-
+        // creating observable that fetches from both urls simultaneosly
+        // zip operator
+        Observable<JSONObject> zipped  = Observable.zip(fetchComments, fetchPosts, new Func2<JSONArray, JSONArray, JSONObject>() {
+            JSONObject jsonObject;
+            @Override
+            public JSONObject call(JSONArray jsonArray, JSONArray jsonArray2) {
+                Log.d("ZIPPED","length of comments "+jsonArray.length()+" "+getTime()+" length of posts "+jsonArray2.length()+" "+getTime());
+                try {
+                    jsonObject = new JSONObject();
+                    jsonObject.put("comments",jsonArray);
+                    jsonObject.put("posts",jsonArray2);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return jsonObject;
+            }
+        });
+        zipped.subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(getObserver());
         // wait for 5 seconds
         /*
         * Code to make Android wait for 5 seconds
@@ -134,21 +156,38 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 //ping all the 4 urls directly
                 Log.d("CHECK","here");
-               // jsonArrayComments = fetchData(urls[0]);
-               // Log.d("CHECK",jsonArrayComments.toString());
-                //jsonArrayComments = fetchData(urls[0],jsonArrayComments);
-                new DownloadTask().execute(urls[0]);
-                /*JSONArray test = new JSONArray();
-                test = fetcchDataOkHttp(urls[0]);
-                if (test!=null){
-                    Log.d("TEST",test.length()+" was not null");
-                }
-                else {
-                    Log.d("TEST"," was null");
-                }*/
             }
         },5000);
 
+    }
+
+    private Observer<JSONObject> getObserver (){
+        return new Observer<JSONObject>() {
+            @Override
+            public void onCompleted() {
+                // observer(zipped) has finished emmitting data
+                endSaveForUrl_comments.append(getTime());
+                endSaveForUrl_posts.append(getTime());
+                Log.d("OBSERVER"," "+"completed");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("OBSERVER",e.getMessage()+"");
+                Toast.makeText(context,e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNext(JSONObject jsonObject) {
+                //called each time the observer emits the data
+                try {
+                    Log.d("OBSERVER",jsonObject.getJSONArray("posts").length()+" "+jsonObject.getJSONArray("comments").length());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
     }
 
 
@@ -172,171 +211,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return null;
     }
-
-    public JSONArray fetchData (String url){
-        RequestFuture<JSONArray> future = RequestFuture.newFuture();
-        // a New Constructor added in the volley library
-        // the null parameter is actually a json array that indicates the json arrya that can be posted with the request made
-        // the last 2 parameters are the listener and the error listener
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,url,null,future,future){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                return super.getParams();
-            }
-
-        };
-        request.setRetryPolicy(new DefaultRetryPolicy(300000,0,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-        ApplicationController.getApplicationControllerInstance().addTorequestQueue(request);
-        try {
-            return future.get(2, TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-            Log.d("CHECK",e.getMessage());
-        } catch (TimeoutException e) {
-//            Log.d("ERROR",e.getMessage());
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-
-    class DownloadTask extends AsyncTask <String, Void, JSONArray> {
-
-        JSONArray res;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            startTimeForUrl_comments.append(getTime());
-        }
-
-        @Override
-        protected JSONArray doInBackground(String... strings) {
-            String url = strings[0];
-            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-                    //endTimeForUrl_comments.append(getTime());
-                    res = response;
-                    Log.d("CHECK REQUEST",""+res.length());
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.d("VOLLEYERROR",error.getMessage());
-                    Toast.makeText(context,error.getMessage(),Toast.LENGTH_SHORT).show();
-                }
-            });
-            ApplicationController.getApplicationControllerInstance().addTorequestQueue(jsonArrayRequest);
-            return res;
-        }
-
-        @Override
-        protected void onPostExecute(JSONArray jsonArray) {
-            super.onPostExecute(jsonArray);
-            endSaveForUrl_comments.append(getTime());
-        }
-    }
-
-    /*class DownloadTask extends AsyncTask <String, Void, JSONArray> {
-
-        JSONArray jsonArray;
-        @Override
-        protected void onPostExecute(JSONArray aVoid) {
-            super.onPostExecute(aVoid);
-           *//* *//**//**//**//*List<Comments> commentsList = new Select().from(Comments.class).execute();
-            for (int i = 0; i < commentsList.size();i++){
-                Log.d("TABLE",commentsList.get(i).name +" "+i);
-            }*//*
-
-        }
-
-        @Override
-        protected JSONArray doInBackground(final String... strings) {
-            String url = urls[0];
-            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-               /*//*Log.d("CHECK", response.length()+" length of response");
-                    // Parsing the JSON ARRAY
-                    String jsonResponse = "";
-                    ActiveAndroid.beginTransaction();
-                    try {
-                    for (int i = 0; i < response.length(); i++){
-                            if (strings[0].equals(urls[0])){
-                                JSONObject comment = (JSONObject) response.get(i);
-                                int postId = comment.getInt("postId");
-                                int id = comment.getInt("id");
-                                String name = comment.getString("name");
-                                String email = comment.getString("email");
-                                String body = comment.getString("body");
-                                Comments comments = new Comments(postId,name,email,body);
-                                comments.save();
-                                jsonResponse += " "+body+" "+email+" "+name+" "+postId;
-                            }
-                            else if (strings[0].equals(urls[1])){
-                                JSONObject photo = (JSONObject) response.get(i);
-                                int albumnId = photo.getInt("albumnId");
-                                int id = photo.getInt("id");
-                                String title = photo.getString("title");
-                                String url = photo.getString("url");
-                                String thumbnailUrl = photo.getString("thumbnailUrl");
-                                Photos photos = new Photos(albumnId,title,url,thumbnailUrl);
-                                photos.save();
-                            }
-                            else if (strings[0].equals(urls[2])){
-                                JSONObject todo = (JSONObject) response.get(i);
-                                int userId = todo.getInt("userId");
-                                int id  = todo.getInt("id");
-                                String title = todo.getString("title");
-                                boolean completed = todo.getString("completed").equals("true")? true:false;
-                                jsonResponse += " "+completed+" "+title+" "+userId;
-                                Todos todos = new Todos(userId,title,completed);
-                                todos.save();
-                            }
-                            else if (strings[0].equals(urls[3])){
-                                JSONObject post = (JSONObject) response.get(i);
-                                Log.d("CHECKOBJECT",post.toString());
-                                int userId;
-                                if (post.has("userId")){
-                                    userId = post.getInt("userId");
-                                    Log.d("POSTS",userId+" not null");
-                                }
-                                else{
-                                    userId = 0;
-                                    Log.d("POSTS",userId+"is null");
-                                }
-                                int id = post.getInt("id");
-                                String title = post.getString("title");
-                                String body = post.getString("body");
-                                jsonResponse += " "+body+" "+title+" "+userId;
-                                Posts posts = new Posts(userId,title,body);
-                                posts.save();
-                            }
-                        }
-                        ActiveAndroid.setTransactionSuccessful();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    finally{
-                        ActiveAndroid.endTransaction();
-                    }/*//*
-                    jsonArray = response;
-                    Log.d("CHECK",jsonArray.toString());
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.e("CHECK",error.getMessage());
-                    Toast.makeText(context,error.getMessage(),Toast.LENGTH_SHORT).show();
-                }
-            });
-            ApplicationController.getApplicationControllerInstance().addTorequestQueue(jsonArrayRequest);
-            return jsonArray;
-        }
-    }*/
 
 }
